e2afd4b9632ea234d5787049eed5fc1481db5b20
suhua <hua@hzmra.com>
2020-01-10 11:19:14 +0800

    调整

--- src/main/java/com/ymw/love/system/util/RSAEncrypt.java
+++ src/main/java/com/ymw/love/system/util/RSAEncrypt.java
@@ -18,7 +18,7 @@ import org.apache.commons.codec.binary.Base64;
 
 /**
 * @author  作者 ：suhua
-* @date 创建时间：2019年10月21日 下午5:06:12
+* @date 创建时间：2019年10月21日
 *类说明：
 */
 public class RSAEncrypt {
@@ -27,30 +27,17 @@ public class RSAEncrypt {
 		//生成公钥和私钥
 		genKeyPair();
 		//加密字符串
-		String message = "{\"codeType\": 2,\"key\": \"1886074950\",\"phone\": \"15475464754\",\"codeType\": 2,\"key\": \"1886074950\",\"phone\": \"15475464754\"}";
+		String message = "{\"phone\":\"18211292414\",\"password\":\"1234567899996325\",\"code\":\"123456\",\"openid\":\"o3xfdt4c2cnVFsJYIEojNJAn5RYadienfis\",\"inviteCode\":\"12345678941\"}";
 		System.out.println("随机生成的公钥为:" + keyMap.get(0));
 		System.out.println("随机生成的私钥为:" + keyMap.get(1));
 		
-		//String messageEn = encrypt(message,keyMap.get(0));
-		//System.out.println(message + "\t加密后的字符串为:" + messageEn);
-		//String messageDe = decrypt(messageEn,keyMap.get(1));
-		//System.out.println("还原后的字符串为:" + messageDe);
+		String messageEn = encrypt(message,keyMap.get(0));
+		System.out.println(message + "\t加密后的字符串为:" + messageEn);
+		String messageDe = decrypt(messageEn,keyMap.get(1));
+		System.out.println("还原后的字符串为:" + messageDe);
 		
-		//lpyXKHHuwueDg7a3DKqC/K3Djt4ukaA64nOWZqDC7Ukz1dvyfmxFVm3CcfJwWuJWBg8JX5PA4asH3oC752SKHQ==
-		
-		
+	
 
-		
-		String messageEns="hZKdyGtxHLSyJv/M+iI1xh033fHv0PEaVMvqPVGwRaSdm5D+9bcm+F22ZW9gHw0wCFI9KEnwPsqPgYCj+Yl3CiuwiDgOOfBFhnWwULo2vlxkNeWY7pq2n0F29g4M1Hnphu+Afuqf7jdmxxE2acTFuU6iR4RLWFm1bOcx5DIbr3c=";
-		
-		//String k="MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAIiR1/kBqyoUC3Jv2Fp3HepSqp6ReU1JN9qENQ9BRWi4zp5E5PH0KApXyPdBYs7BY4Swjl7LZ/DHOAZY4o9oGtECAwEAAQ==";
-		String v="MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAJbbo+GaTyxNhtuQAhrKAaoQoI6GMicLFBzJmGh4HBpDRuS5wd22BR0IoXsbn4uZzFPIInZ2Rfg/tCz6ywvLSHKbUg27RuoVW+3D62MlwVAReZPV82nXm1/g92MEaYPKkZri2UtJEZNxOd8d5VmFzNNOwI/y/q8DT6oNgjA3tA9/AgMBAAECgYEAlnK/kovA7871clzBLJRPapqgdOMyeyuaSEdovq8iX7K+rfbvd7xChz0HRm47BA9C7j9G11uG1lg5yS3od3+pZE82zs0xRijxv/E+omanq8qQEbW9PyZqDlXgWeLtMeOzMny37O4Z4DEEb9lrqw8WiEJsk6GGODh8B42jCqz2JcECQQDglpnCnrqPlwp4WkNMCO4mU6nCOuajrhqoW84EfgEb9vhTIbq6bSxVDAIhfft7R8dTs2jRkpK5JQ0lAKK3ziLhAkEAq/Ue08meSSIN0vor1beK5D5HppwdrhKEOTxUYPaGe1YZ/V/4WGATk4fBRWJPEGiG6heXSt7q7FR70fZe9LbeXwJAT6v9VfZbNm8djr5umz2bxkrtsPKWOUCdQlZ3Ck7s88+0HkGb7WCnVnBRHTKFESHQi1fc+mOl2zpSpGrKTqw44QJBAIHOR7cYO6R5UHYy42QxaWdinDPpJX6B+844/Hk1n8768COUjqQn4p87m2oFDCoWvcF4jXy3Sh+cptuYX5XbdYECQC3p7RNmb2JyfrggppOKxQ374kzaeISFS0TiMPRWt2zw8HXH5Oe1+Kql2QA10P4tGVccTQRm0Qi/4ZBs1g+SlHM=";
-		
-		//String messageEns = encrypt(message,k);
-		//System.out.println(message + "\t加密后的字符串为:" + messageEns);
-	String messageDes = decrypt(messageEns,v);
-	  System.out.println("还原后的字符串为:" + messageDes);
-		
 	
 	}
 
@@ -62,7 +49,7 @@ public class RSAEncrypt {
 		// KeyPairGenerator类用于生成公钥和私钥对，基于RSA算法生成对象  
 		KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");  
 		// 初始化密钥对生成器，密钥大小为96-1024位  
-		keyPairGen.initialize(1024,new SecureRandom());  
+		keyPairGen.initialize(1280,new SecureRandom());  
 		// 生成一个密钥对，保存在keyPair中  
 		KeyPair keyPair = keyPairGen.generateKeyPair();  
 		RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();   // 得到私钥  
